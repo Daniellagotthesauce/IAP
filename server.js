0c1b106de5fddfb385e3f0328c64aa0b3b545271
Steve.K.Y <122028234+Steven-Yabann@users.noreply.github.com>
2024-11-27 23:57:46 +0300

    fixed cors

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:4000',
+    credentials: true,              
+}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
